c27a4be4d74b63d4aed14899ebf9e54e6c617305
ian ferreira <100284532+itupii@users.noreply.github.com>
2024-01-29 15:30:13 -0300

    correcao

--- api/app.py
+++ api/app.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("/public/index.html")
 
 @app.route("/contatos")
 def contatos():
-    return render_template("contatos.html")
+    return render_template("/public/contatos.html")
 
 @app.route("/linguagens")
 def linguagens():
-    return render_template("linguagens.html")
+    return render_template("/public/linguagens.html")
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    return render_template("/public/projects.html")
 
 @app.route("/projectPessoal")
 def projectPessoal():
-    return render_template("projectPessoal.html")
+    return render_template("/public/projectPessoal.html")
 
 @app.route("/sobremim")
 def sobremim():
-    return render_template("sobremim.html")
+    return render_template("/public/sobremim.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
