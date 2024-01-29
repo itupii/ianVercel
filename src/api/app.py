23b91dbb4513ffb287cb202355e577e9a628eec7
ian ferreira <100284532+itupii@users.noreply.github.com>
2024-01-29 13:14:00 -0300

    correcao

--- src/api/app.py
+++ src/api/app.py
@@ -6,27 +6,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('/public/index.html')
+    return render_template('/templates/index.html')
 
 @app.route('/projects')
 def projects():
-    return render_template('/public/projects.html')
+    return render_template('/templates/projects.html')
 
 @app.route('/projectPessoal')
 def projectPessoal():
-    return render_template('/public/projectPessoal.html')
+    return render_template('/templates/projectPessoal.html')
 
 @app.route('/sobremim')
 def sobremim():
-    return render_template('/public/sobremim.html')
+    return render_template('/templates/sobremim.html')
 
 @app.route('/linguagens')
 def linguagens():
-    return render_template('/public/linguagens.html')
+    return render_template('/templates/linguagens.html')
 
 @app.route('/contatos')
 def contatos():
-    return render_template('/public/contatos.html')
+    return render_template('/templates/contatos.html')
 
 
 if __name__ == '__main__':
