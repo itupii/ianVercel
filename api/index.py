d65199057854d1e192b86730b675f81268963e88
ian ferreira <100284532+itupii@users.noreply.github.com>
2024-01-29 13:45:38 -0300

    correcao

---
+++ api/index.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/linguagens")
+def linguagens():
+    return render_template("linguagens.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/projectPessoal")
+def projectPessoal():
+    return render_template("projectPessoal.html")
+
+@app.route("/sobremim")
+def sobremim():
+    return render_template("sobremim.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
